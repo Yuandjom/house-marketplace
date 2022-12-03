5a3fce343d959997816b85ae64a0e5f019886694
Yuandjom <ziyangjohn@gmail.com>
2022-12-03 12:00:55 +0800

    Pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import ForgotPassword from './pages/ForgotPassword';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+
 function App() {
   return (
     <>
-      <h1>My App</h1>  
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element= {<SignIn />} />
+          <Route path='/sign-in' element= {<SignIn />} />
+          <Route path='/sign-up' element= {<SignUp />}/>
+          <Route path='/forget-password' element= {<ForgotPassword />}/>
+        </Routes>
+      </Router>
+      {/**Navbar */}
+      
     </>
   );
 }
